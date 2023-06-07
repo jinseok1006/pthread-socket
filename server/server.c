03eba348f07193a0d8edeededae847a4bca9bb67
jinseok1006 <jinseok@naver.com>
2023-06-07 21:20:26 +0900

    update

--- server/server.c
+++ server/server.c
@@ -14,12 +14,14 @@ const char err_msg[ERR_SIZE] = "FILE_NO";
 const char ok_msg[ERR_SIZE] = "FILE_OK";
 
 int connection(int);
-int readAndPass(int, const char[]);
+int readAndSend(int, const char[]);
+void *run(void *);
 
 int main(void) {
   int s_sock, c_sock;
   struct sockaddr_in server_addr, client_addr;
   socklen_t c_addr_size;
+  pthread_t tid;
 
   s_sock = socket(AF_INET, SOCK_STREAM, 0);
   if (s_sock == -1) {
@@ -27,9 +29,11 @@ int main(void) {
     exit(1);
   }
 
+  // 포트 재사용
   int option = 1;
   setsockopt(s_sock, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
 
+  // 서버 주소 설정
   bzero(&server_addr, sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(SERVERPORT);
@@ -43,40 +47,45 @@ int main(void) {
 
   listen(s_sock, 1);
 
-  // 클라이언트 소켓 통신 성공
   c_addr_size = sizeof(client_addr);
-  c_sock = accept(s_sock, (struct sockaddr *)&client_addr, &c_addr_size);
-  if (c_sock == -1) {
-    perror("accept");
-    exit(1);
+
+  // 클라이언트 accept 무한 대기
+  while (1) {
+    c_sock = accept(s_sock, (struct sockaddr *)&client_addr, &c_addr_size);
+    if (c_sock == -1) {
+      perror("accept");
+      exit(1);
+    }
+    int *new_c_sock = malloc(sizeof(int));
+    *new_c_sock = c_sock;
+
+    // 연결 수립시 쓰레드로 생성후 소켓 넘김
+    pthread_create(&tid, NULL, run, new_c_sock);
+    pthread_detach(tid);
   }
 
-  // 디버그용
-  printf("s_sock=%d, c_sock=%d\n", s_sock, c_sock);
-  printf("client IP addr=%s port=%d\n", inet_ntoa(client_addr.sin_addr),
-         ntohs(client_addr.sin_port));
-
-  connection(c_sock);
-
-  close(c_sock);
   close(s_sock);
 
   return 0;
 }
 
+// 연결이 수립하면 클라이언트로 부터 파일명을 받고 있는지 확인하는 작업
 int connection(int c_sock) {
-  char path[BUFFSIZE];
-  char buf[BUFFSIZE];
+  char path[BUFFSIZE] = "./data/";
+  char filename[BUFFSIZE] = {0};
+  char buf[BUFFSIZE] = {0};
 
   // 파일명 받아오기
-  if (recv(c_sock, path, BUFFSIZE, 0) == -1) {
-    perror("recv");
+  if (recv(c_sock, filename, BUFFSIZE, 0) == -1) {
+    perror("filename recv");
     exit(1);
   }
 
-  // 파일이 존재하면 OK, 없으면 NO ITEM 발송
+  strcat(path, filename);
+
+  // 클라이언트에게 파일이 존재하면 OK, 없으면 NO ITEM 발송
   if (access(path, F_OK) != 0) {
-    printf("클라언트에서 요청한 파일 %s가 서버에 없어요\n", path);
+    printf("%d: 클라이언트에서 요청한 파일 %s가 서버에 없어요\n", c_sock, path);
     if (send(c_sock, err_msg, sizeof(err_msg), 0) == -1) {
       perror("err send");
       exit(1);
@@ -89,19 +98,19 @@ int connection(int c_sock) {
     }
   }
 
-  // 클라이언트 수신 가능 여부 받기
+  // 클라이언트 수신 가능 여부 받기(한번에 send방지)
   if (recv(c_sock, buf, BUFFSIZE, 0) == -1) {
-    perror("recv");
+    perror("ok recv");
     exit(1);
   }
-  printf("[Client]: %s\n", buf);
+  printf("%d: Client says %s\n", c_sock, buf);
 
-  readAndPass(c_sock, path);
-
-  return 0;
+  // 파일이 존재하고 클라이언트가 수신하는데 문제없다면 파일 읽고 전송
+  return readAndSend(c_sock, path);
 }
 
-int readAndPass(int c_sock, const char path[]) {
+// 파일을 열고 읽은다음 클라이언트에게 전송
+int readAndSend(int c_sock, const char path[]) {
   FILE *fp;
   int read_count;
   char buf[BUFFSIZE + 1] = {0};
@@ -112,18 +121,33 @@ int readAndPass(int c_sock, const char path[]) {
     memset(buf, 0, BUFFSIZE);
     read_count = fread(buf, 1, BUFFSIZE, fp);
 
+    // 더 이상 읽은 값이 없으면 종료
     if (read_count <= 0)
       break;
 
+    // 클라이언트에게 읽은 만큼 전달
     if (send(c_sock, buf, read_count, 0) == -1) {
       perror("file send");
       exit(1);
     }
-    // printf("%d:%s", count++, buf);
   }
 
-  printf("End!!\n");
+  printf("%d: %s send successful\n", c_sock, path);
+
   fclose(fp);
 
   return 0;
+}
+
+// 소켓 연결 수립시 실제 통신을 담당하는 쓰레드
+void *run(void *args) {
+  int c_sock = *(int *)args;
+  printf("New Thread c_sock:%d\n", c_sock);
+
+  connection(c_sock);
+
+  printf("close Thread c_sock:%d\n", c_sock);
+  close(c_sock);
+  free(args);
+  pthread_exit(NULL);
 }
